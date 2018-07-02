d8867467ab809cddc55a65b8e816b0a91f3ffb41
Cory Wyszynski <cp.wyszynski@gmail.com>
2018-07-01 23:37:47 -0700

    Add second argv

--- ex14/ex14.py
+++ ex14/ex14.py
@@ -1,9 +1,9 @@
 from sys import argv
 
-script, user_name = argv
+script, user_name, dimension = argv
 prompt = '> '
 
-print(f"Hi {user_name}, I'm the {script} script.")
+print(f"Hi {user_name}, from {dimension}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
@@ -18,4 +18,6 @@ print(f"""
 Alright, so you said {likes} about liking me.
 You live in {lives}. Not sure where that is. 
 And you have a {computer} computer. Nice.
+That is all well and good {user_name}, from {dimension}.
+I like you.
 """)
