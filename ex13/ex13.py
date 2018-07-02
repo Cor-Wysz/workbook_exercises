2d89eb7a1b1b767d881ef73feb0d6953442415e5
Cory Wyszynski <cp.wyszynski@gmail.com>
2018-07-01 23:31:02 -0700

    Drill ex13

---
+++ ex13/ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+crow = input("What do you know Jon Snow?")
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("You know nothing", third)
+
