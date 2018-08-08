051ef47ca471c51a01a1c60ec73f1957e19c230c
Cory Wyszynski <cp.wyszynski@gmail.com>
2018-08-07 20:50:31 -0700

    Complete ex22

---
+++ ex22/ex22.py
@@ -0,0 +1,57 @@
+Exercise 1
+# print()
+
+Exercise 2
+# Comments
+
+Exercise 3
+# modulus
+# PEMDAS
+
+Exercise 4
+# Variables
+
+Exercise 5
+# Format Strings - print(f"
+# curly braces
+
+Exercise 6
+# bouillons
+
+Exercise 9
+# \n
+# print("""
+
+Exercise 10
+# \t
+# refactor
+
+Exercise 11
+# input()
+
+Exercise 13
+# argv
+
+Exercise 15
+# open()
+# .read()
+
+Exercise 16
+# open, 'w'
+# .write()
+# .close()
+
+Exercise 17
+# echo
+# cat
+
+Exercise 18
+# def
+# *args
+# run or call
+
+Exercise 20
+# seek(0)
+
+Exercise 21
+# return
